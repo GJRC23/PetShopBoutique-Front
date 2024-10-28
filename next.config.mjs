28156f5a73f1131f6b0b52e894ffbf10607cd882
GJRC23 <gastuconsoli8@gmail.com>
2024-10-28 14:43:23 -0300

    pagos acomodado

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: false,
   output: 'export',
   images: {
     unoptimized: true,
   },
-
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/pages/pagosFaqTnC.js
+++ src/pages/pagosFaqTnC.js
@@ -11,11 +11,11 @@ const PagosFAQTnC = () => {
         {/* Medios de pago */}
         <section className="mb-12 md:mb-16">
           <h3
-            className={`${styles.pagosTitle} text-4xl md:text-6xl font-bold mb-4 text-center md:text-left`}
+            className={`${styles.pagosTitle} text-4xl md:text-6xl mb-4 text-center md:text-left`}
           >
             MEDIOS DE PAGO
           </h3>
-          <h3 className="mb-2 font-semibold text-4x1 md:text-2x1">
+          <h3 className="mb-2 text-4x1 md:text-2x1">
             Aceptamos los siguientes medios de pago:
           </h3>
           <ul className="ml-5">
@@ -37,12 +37,12 @@ const PagosFAQTnC = () => {
         {/* Preguntas frecuentes */}
         <section className="mb-12 md:mb-16">
           <h3
-            className={`${styles.pagosTitle} text-4xl md:text-6xl font-bold mb-4 text-center md:text-left`}
+            className={`${styles.pagosTitle} text-4xl md:text-6xl mb-4 text-center md:text-left`}
           >
             PREGUNTAS FRECUENTES
           </h3>
           <div className="mb-4">
-            <h3 className="font-semibold text-4x1 md:text-2x1">
+            <h3 className="text-4x1 md:text-2x1">
               ¿ Cuál es su horario ?
             </h3>
             <p className="text-base md:text-lg">
@@ -50,7 +50,7 @@ const PagosFAQTnC = () => {
             </p>
           </div>
           <div className="mb-4">
-            <h3 className="font-semibold text-4x1 md:text-2x1">
+            <h3 className="text-4x1 md:text-2x1">
               ¿ Tienen local físico ? ¿ Dónde ?
             </h3>
             <p className="text-base md:text-lg">
